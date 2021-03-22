ef8f85c3db6f50280a0d6a3d8e2c7d5663d7ef02
森本拓朗 <ujimattya.mt@gmail.com>
2021-03-22 22:57:07 +0900

    githubpagesデプロイ修正試し2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  assetPrefix: '/githubpages-nextjs-tailwind-test/',
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/githubpages-nextjs-tailwind-test/_next/static/images/',
+  }],
+  {
+    basePath: '/githubpages-nextjs-tailwind-test',
+    assetPrefix: '/githubpages-nextjs-tailwind-test/',
+    env,
+  },
+]);
